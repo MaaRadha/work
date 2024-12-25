a818328ebf5c98d3aec2494d32d5ab95245d3977
MaaRadha <Omyoga2016@outlook.com>
2024-12-25 20:14:03 +0100

    added border top

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full text-gray-700 my-4">
+    <div className="w-full text-gray-700 my-4 border-t-4 max-w-7xl mx-auto container">
       <footer className="pt-10 py-5">
         <div className=" text-center">
           <span className="font-bold"> &copy; Rohit kumar Amdahl, </span>
