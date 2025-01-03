7d5d5d046f72a5f5195fe53bb2a9abe27f3b386e
MaaRadha <Omyoga2016@outlook.com>
2025-01-03 22:23:33 +0100

    added github link

--- src/components/work/ProjectList.jsx
+++ src/components/work/ProjectList.jsx
@@ -72,7 +72,7 @@ const ProjectList = () => {
           </p>
           <div className=" py-7 px-3 w-full max-w-[260px] flex items-center ">
             <a
-              href="mailto:rohit_designer@outlook.com"
+              href="https://github.com/RohitAmdahl?tab=repositories"
               className="px-4 py-2 rounded-full flex items-center gap-2  
         shadow-[-5px_-5px_10px_rgba(255,_255,_255,_0.8),_5px_5px_10px_rgba(0,_0,_0,_0.25)]
         transition-all hover:shadow-[-1px_-1px_5px_rgba(255,_255,_255,_0.6),_1px_1px_5px_rgba(0,_0,_0,_0.3),inset_-2px_-2px_5px_rgba(255,_255,_255,_1),inset_2px_2px_4px_rgba(0,_0,_0,_0.3)] hover:text-violet-500 hover:ring hover:ring-violet-500 text-center text-zinc-800"
